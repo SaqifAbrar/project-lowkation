b6e381996c25773f382414cececc8e692f4d5af3
Saqif Abrar <saqif.abrar@gmail.com>
2021-12-17 17:41:01 -0500

    updated routes

--- app_node/src/routes/routes.js
+++ app_node/src/routes/routes.js
@@ -1,2 +1,11 @@
 const express = require("express");
-const users = require("users");
+const users = require("../controllers/users");
+const categories = require("../controllers/users");
+const locations = require("../controllers/users");
+
+module.exports = function (app) {
+    app.use(express.json());
+    app.use("/users", users);
+    app.use("/categories", categories);
+    app.use("/locations", locations);
+};
