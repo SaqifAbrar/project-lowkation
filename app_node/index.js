7d9b1c5e95dbcdeac9a556602a1328a1aaa6a15e
LAVANNNJAMES <lavanisawesome@gmail.com>
2021-12-17 11:24:09 -0500

    Change in json and added firebase to js file

---
+++ app_node/index.js
@@ -0,0 +1,13 @@
+import {initializeApp } from 'firebase/app'; 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAhr2L_E6l5M9DfcaY6ASPZ_8tY_wvf7vo",
+    authDomain: "lowcation-91138.firebaseapp.com",
+    projectId: "lowcation-91138",
+    storageBucket: "lowcation-91138.appspot.com",
+    messagingSenderId: "1011043694597",
+    appId: "1:1011043694597:web:348d41945fb7476389446c"
+}
+
+initializeApp(firebaseConfig); 
+
