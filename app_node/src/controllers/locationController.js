a4fa80b9f80542c7023521506beb097bd5e37621
Saqif Abrar <saqif.abrar@gmail.com>
2021-12-18 20:01:13 -0500

    updated CSS for search page

--- app_node/src/controllers/locationController.js
+++ app_node/src/controllers/locationController.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const locationData = require("../models/locations");
-const firestoreClient = require("../../firestoreClient")
+const firestoreClient = require("../../firestoreClient");
 
 const locations = async (req, res) => {
+    let realtimeLocation = {};
 
-    let realtimeLocation = {}
-     
-    const data = await firestoreClient()
-    .then((object) => {
-        realtimeLocation = object
-    })
-        
-        
+    const data = await firestoreClient().then((object) => {
+        realtimeLocation = object;
+    });
+
     console.log("sending data");
-    res.send(realtimeLocation); 
+    res.send(realtimeLocation);
+};
+
+const searchLocations = (req, res) => {
+    const search = req.query || req.body;
+    const field = search.input;
+
+    const data = locationData.filter((location) => location.name === field);
+
+    res.send(data);
 };
 
 module.exports = {
-    locations
+    locations,
 };
