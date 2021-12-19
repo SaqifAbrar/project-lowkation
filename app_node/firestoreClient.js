3376328d336362b10ad58f8cec9f5aaa272f3320
Rithik <rithik.chdry@gmail.com>
2021-12-18 21:44:00 -0500

    fixed search from name

--- app_node/firestoreClient.js
+++ app_node/firestoreClient.js
@@ -1,67 +1,72 @@
-const admin = require('firebase/app');
-const firebaseFUCKING = require('firebase/firestore')
-
-
+const admin = require("firebase/app");
+const firebaseFUCKING = require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhr2L_E6l5M9DfcaY6ASPZ_8tY_wvf7vo",
-  authDomain: "lowcation-91138.firebaseapp.com",
-  projectId: "lowcation-91138",
-  storageBucket: "lowcation-91138.appspot.com",
-  messagingSenderId: "1011043694597",  
-  appId: "1:1011043694597:web:348d41945fb7476389446c" 
-}
+    apiKey: "AIzaSyAhr2L_E6l5M9DfcaY6ASPZ_8tY_wvf7vo",
+    authDomain: "lowcation-91138.firebaseapp.com",
+    projectId: "lowcation-91138",
+    storageBucket: "lowcation-91138.appspot.com",
+    messagingSenderId: "1011043694597",
+    appId: "1:1011043694597:web:348d41945fb7476389446c",
+};
 
+const app = admin.initializeApp(firebaseConfig);
+const db = firebaseFUCKING.getFirestore(app);
 
+const colRef = firebaseFUCKING.collection(db, "Location");
 
-const app = admin.initializeApp(firebaseConfig)
-const db = firebaseFUCKING.getFirestore(app)
+async function getAllLocationsNoRank() {
+    let locations = new Array();
+    let userRank = 10;
 
-const colRef = firebaseFUCKING.collection(db,"Location");
-
-
-async function getAllLocationsNoRank(){ 
-    let locations = new Array()
-    let userRank = 10
-
-   
-   await firebaseFUCKING.getDocs(colRef)
-    .then((snapshot) => {
-       
-       snapshot.docs.forEach((doc) => {
-            if(userRank >= doc.get("rank")){
-                locations.push({...doc.data(), id: doc.id})
-
-            }
-       })
-
-     
-    })
-    .catch((err) => console.log(err.message))
-    
-    console.log(locations)
-
-    return (locations);   
-}
-
-
-function searchBasedOffName (name) {
-    const q = firebaseFUCKING.query(colRef, firebaseFUCKING.where("name", "==", name.toLowerCase()))
-    let locations = []
-    firebaseFUCKING.onSnapshot(q, (snapshot) => {
-        snapshot.docs.forEach((doc) => {
-                locations.push({...doc.data(), id: doc.id})
+    await firebaseFUCKING
+        .getDocs(colRef)
+        .then((snapshot) => {
+            snapshot.docs.forEach((doc) => {
+                if (userRank >= doc.get("rank")) {
+                    locations.push({ ...doc.data(), id: doc.id });
+                }
+            });
         })
-        console.log(locations)
-    })
+        .catch((err) => console.log(err.message));
 
-    return(locations)
+    console.log(locations);
+
+    return locations;
+}
+
+async function searchBasedOffName(name) {
+    let locations = new Array();
+    let userRank = 10;
+
+    const q = firebaseFUCKING.query(
+        colRef,
+        firebaseFUCKING.where("name", "==", name.toLowerCase())
+    );
+
+    await firebaseFUCKING.getDocs(q).then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+            locations.push({ ...doc.data(), id: doc.id });
+        });
+        // console.log(locations);
+    });
+
+    // firebaseFUCKING.onSnapshot(q, (snapshot) => {
+    //     snapshot.docs.forEach((doc) => {
+    //         locations.push({ ...doc.data(), id: doc.id });
+    //     });
+    //     console.log(locations);
+    // });
+
+    console.log(locations);
+
+    return locations;
 }
 
 // getAllLocationsNoRank()
 // searchBasedOffName("RITZY HOUSE")
 
 module.exports = {
-  getAllLocationsNoRank,
-  searchBasedOffName
+    getAllLocationsNoRank,
+    searchBasedOffName,
 };
