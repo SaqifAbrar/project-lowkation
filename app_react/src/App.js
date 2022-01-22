a7a68b0360d4ce7ef96cd373294288a2d25599c8
Rithik <rithik.chdry@gmail.com>
2022-01-21 23:01:40 -0500

    idek

--- app_react/src/App.js
+++ app_react/src/App.js
@@ -4,22 +4,22 @@ import NavBar from "./components/navbar";
 import FootBar from "./components/footbar";
 import Landing from "./components/homePage/landing";
 import Search from "./components/searchPage/search";
-import LocationPage from "./components/locationPage/locationPage";
+import LowkationMap from "./components/locationPage/lowkationMap";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <NavBar />
-            <Routes>
-                <Route path="/home" element={<Landing />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/location" element={<LocationPage />} />
-                <Route path="/" element={<Navigate replace to="/home" />} />
-            </Routes>
-            <FootBar />
-        </div>
-    );
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/home" element={<Landing />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/location" element={<LowkationMap />} />
+        <Route path="/" element={<Navigate replace to="/home" />} />
+      </Routes>
+      <FootBar />
+    </div>
+  );
 }
 
 export default App;
